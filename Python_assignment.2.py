7442892d613555b6e11089f17e91894e3e7f23ae
Akshay kumar Wankhade <55886988+akshay13127@users.noreply.github.com>
2020-10-19 18:41:14 +0530

    Add files via upload

---
+++ Python_assignment.2.py
@@ -0,0 +1,4 @@
+fname = input("Type your First name :")
+lname = input ("Type your Last name :")
+
+print ("Hey! " + lname + " " + fname )
---
+++ Python_assignment.3.py
@@ -0,0 +1,5 @@
+diameter = int(input())
+r = diameter / 2 
+pia = 3.1415
+Volume = 4/3 * pia * r**3
+print (Volume)
